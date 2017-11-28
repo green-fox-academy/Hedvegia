4f002db92675acb74b6dbec3b96d5401bee86c70
Hedvegia <hedi9507@gmail.com>
2017-11-28 17:00:11 +0100

    exercise17-19

---
+++ week-02/day-2/exercise17.py
@@ -0,0 +1,7 @@
+
+# - Create a variable named `abc`
+#   with the following content: `["first", "second", "third"]`
+# - Swap the first and the third element of `abc`
+
+abc = ["first", "second", "third"]
+print(abc[ : :-1])
