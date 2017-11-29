3f41a8a69a3999e03459ade03ce0ede4a4d39976
hedvegia <hedi9507@gmail.com>
2017-11-29 11:43:46 +0100

    string

--- week-02/day-3/s01.py
+++ week-02/day-3/s01.py
@@ -3,5 +3,6 @@
 # Expected ouput: In a galaxy far far away
 
 
-change = "In a dishwasher far far away"
-print("In a ", "galaxy ", "far far away") 
+original = "In a dishwasher far far away"
+change = original[ : 5] + 'galaxy' + original[15: ]
+print(change)
