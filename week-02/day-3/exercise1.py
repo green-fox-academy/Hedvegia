653d00dd641e92545c28e994d2aa54adef60078f
hedvegia <hedi9507@gmail.com>
2017-11-29 09:13:26 +0100

    day-3

---
+++ week-02/day-3/exercise1.py
@@ -0,0 +1,7 @@
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
+
+
+var = "In a dishwasher far far away"
+print("In a ", "galaxy ", "far far away") 
