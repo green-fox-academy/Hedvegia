621ab9a66b76a31e42775765f2e77fd778532251
hedvegia <hedi9507@gmail.com>
2018-01-15 16:28:22 +0100

    week-08/day1

---
+++ week-08/day1/sharpie.js
@@ -0,0 +1,28 @@
+//We should know about each sharpie:
+//  color (which should be a string)
+//  width (which will be a number)
+//  inkAmount (another number)
+//When instantiating a Sharpie, we need to specify the color and the width
+//Every sharpie is created with a default 100 as inkAmount
+//We can use() the sharpie objects
+//  which decreases inkAmount by the width
+//Write a loop that consumes all the sharpie's ink.
+//  Make sure your loop works with any inkAmount, so your code figures out when it's out of ink.
+
+class Sharpie{
+    constructor(type, color, width) {
+        this.color = color;
+        this.width = 0,
+        this.inkAmount = 100;
+    }
+    use () {
+        this.inkAmount -= this.width;
+        console.log(this.inkAmount);
+    }
+}
+
+let sharpie = new Sharpie('sharpie', 'blue', 10);
+
+while (sharpie.inkAmount != 0) {
+    sharpie.use();
+};
