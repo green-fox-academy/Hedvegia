ca9cc7bf877b9cbd4b2d3b919b93246bc666ba4b
Hedvegia <hedi9507@gmail.com>
2018-01-27 19:20:21 +0100

    map

--- week-08/day3/task3.js
+++ week-08/day3/task3.js
@@ -5,16 +5,9 @@ let googleMap = document.getElementsByTagName('iframe')[0];
 function getPosition() {
   let inputCity = document.getElementById('position').value;
   
-  inputCity = inputCity.split(' ')
-  
-  let inputs = inputCity[0] + '+' + inputCity[1];
-  console.log(inputCity[0]);
-  console.log(inputCity[1]);
-  console.log(inputs);
-  
   let httpRequest = new XMLHttpRequest();
 
-  httpRequest.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=' + inputs, true);
+  httpRequest.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=' + inputCity, true);
   httpRequest.setRequestHeader('X-Mashape-Key', 'kNZCeFC5Otmsh0OnupPCIpQNkck7p18x6A3jsnHrkaW8Bpum8V');
   httpRequest.setRequestHeader('Accept', 'application/json');
   httpRequest.send();
@@ -27,19 +20,7 @@ function getPosition() {
       let text = JSON.parse(httpRequest.responseText);
       console.log(text);
 
-      let dataResults = text.Results;
-      console.log(dataResults);
-
-      let latitude = dataResults.lat;
-      let longitude = dataResults.lon;
-      let positionNumbers = document.createElement('div');
-      let top = document.querySelector('.top');
-
-      top.appendChild(positionNumbers);
-
-      positionNumbers.innerText = 'Latitude:' + latitude + ', Longitude:' + longitude;
-
-      let googleMapLink = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAcCN0jWGgLTS_Uru8xDLaZ-GxisowNuIQ&q=' + inputs;
+      let googleMapLink = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAcCN0jWGgLTS_Uru8xDLaZ-GxisowNuIQ&q=' + inputCity;
       
       googleMap.setAttribute('src', googleMapLink);
     } else {
