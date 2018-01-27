c928b7907df6f8a0eee7b989a0ccd7c4cd91ea79
Hedvegia <hedi9507@gmail.com>
2018-01-27 21:45:10 +0100

    appenda, dountil

--- week-09/day3/index.js
+++ week-09/day3/index.js
@@ -22,7 +22,7 @@ app.get('/doubling', function(req, res){
             "result": req.query.input * 2
         }
     }
-    res.json(body);  //this will be the send
+    res.json(body);  
 });
 
 app.get('/greeter', function(req, res){
@@ -40,25 +40,47 @@ app.get('/greeter', function(req, res){
             "welcome_message": "Oh, hi there " + req.query.name + " my dear " + req.query.title + " !"
         }
     }
-    res.json(body);  //this will be the send
+    res.json(body);  
 });
 
-app.get('/append', function(req, res){
+app.get('/appenda/:appendable', function(req, res){
     let body;
-    if (req.query.name === undefined) {
+    if (req.params != null) {
         body = {
-            "error": 'Please provide a name!'
-        }   
-    } else if (req.query.title === undefined) {
-        body = {
-            "error": 'Please provide a title!'
-        }   
-    } else {
-        body = {
-            "welcome_message": "Oh, hi there " + req.query.name + " my dear " + req.query.title + " !"
+            "appended": req.params.appendable + "a"
         }
+        res.json(body);
+    } else {
+        res.status(404);
     }
-    res.json(body);  //this will be the send
 });
 
-app.listen(8001);
+app.post('/dountil/:what', function (req, res) {
+    let body;
+    if (req.params.what != null) {
+      if (req.params.what === 'sum') {
+        let sum = 0;
+        for (let i = 1; i <= req.body.until; i++ ) {
+          sum += i;
+        }
+        body = {
+          "result": sum
+        }
+      } else if (req.params.what === 'factor') {
+        let factor = 0;
+        for (let i = 1; i <= req.body.until; i++ ) {
+          factor *= i;
+        }
+        body = {
+          "result": factor
+      }
+      }
+    } else {
+      body = {
+          "error": "Please provide a number!"
+      }
+    }
+    res.json(body);
+  });
+
+app.listen(8080);
