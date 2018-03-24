156fccf43a2f1f3d9434e2628fa6e2359ec31b7d
Germus Hédi <hedi9507@gmail.com>
2018-03-24 12:48:35 +0100

    refact(test): refactoring the tests

--- week-09/day2/exception.js
+++ week-09/day2/exception.js
@@ -7,7 +7,7 @@
 let  addString = function(str1, str2){
   if (typeof str1 !== 'string') {
     throw new Error(`${str1} is not a string`);
-  } else if (typeof str2 !== 'strin') {
+  } else if (typeof str2 !== 'string') {
     throw new Error(`${str2} is not a string`);
   }
 
